adabe9bd84581f5a4a6ceda4de5115d957578fe6
Shashank1115 <shashankpandey11152004@gmail.com>
2025-06-18 16:05:19 +0530

    vlm thing

--- server/vlm_handler.py
+++ server/vlm_handler.py
@@ -1,39 +1,3 @@
-# import requests
-# from PIL import Image
-# import base64
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-# # HF_TOKEN = os.getenv("HF_TOKEN")
-# HF_TOKEN="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"
-# API_URL = "https://api-inference.huggingface.co/models/nlpconnect/vit-gpt2-image-captioning"
-# HEADERS = {"Authorization": f"Bearer {HF_TOKEN}"}
-
-# def generate_caption(image_path):
-#     print(f"[VLM] Using image: {image_path}")
-
-#     try:
-#         with open(image_path, "rb") as f:
-#             image_bytes = f.read()
-#         print("[DEBUG] Image file successfully read.")
-#     except Exception as e:
-#         print(f"[ERROR] Failed to read image file: {e}")
-#         return f"[Error] Failed to read image: {e}"
-
-#     try:
-#         response = requests.post(API_URL, headers=HEADERS, data=image_bytes)
-#         print(f"[DEBUG] HTTP status code: {response.status_code}")
-#         result = response.json()
-#         print("[VLM] HF Response:", result)
-#     except Exception as e:
-#         print(f"[ERROR] Failed to call HF API: {e}")
-#         return f"[Error] HF API call failed: {e}"
-
-#     if isinstance(result, list) and "generated_text" in result[0]:
-#         return result[0]["generated_text"]
-#     else:
-#         return f"[Error] Captioning failed: {result}"
 
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
